4f5b362a83d4767de1fe84b539b0d69def71089a
memorah <filip.mackov@gmail.com>
2023-06-07 16:10:46 +0200

    edit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -15,13 +16,7 @@ use App\Models\Listing;
 |
 */
 // All Listings
-Route::get('/', function () {
-    return view('listings', [
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 // Single Listing
-Route::get('/listings/{listing}', function (Listing $listing) {
-    return view('listing', ['listing' => $listing]);
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
