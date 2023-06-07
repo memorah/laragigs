39a5b7dbde87859136f37b1f7545373cb7e736c2
memorah <filip.mackov@gmail.com>
2023-06-07 15:14:58 +0200

    merge class

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -7,7 +7,7 @@
 ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-<div class="bg-gray-50 border border-gray-200 p-10 rounded">
+<x-card class="p-10 bg-black">
     <div
         class="flex flex-col items-center justify-center text-center"
     >
@@ -69,6 +69,6 @@
             </div>
         </div>
     </div>
-</div>
+</x-card>
 </div>
 @endsection
