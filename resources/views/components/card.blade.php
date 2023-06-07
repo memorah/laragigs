45f2b42d2b7fb63f732b43104b16d106048f98b1
memorah <filip.mackov@gmail.com>
2023-06-07 15:24:15 +0200

    class mistake fix

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<div {{$attributes->merge([ 'bg-gray-50 border border-gray-200 rounded p-6' => 'class'])}}>
+<div {{$attributes->merge(['class' => 'bg-gray-50 border border-gray-200 rounded'])}}>
     {{$slot}}
 </div>
--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,7 +1,7 @@
 
 @props(['listing'])
 <!-- Item -->
-<x-card>
+<x-card class="p-6" >
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
