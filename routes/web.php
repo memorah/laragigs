c06c9dad85868d9dce5aa3a639a8807d286f61d1
memorah <filip.mackov@gmail.com>
2023-06-22 15:23:19 +0200

    create.blate template

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ use App\Models\Listing;
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
 
-// Single Listing
-Route::get('/listings/{listing}', [ListingController::class, 'show']);
-
 // Show Create Form
 Route::get('/listings/create', [ListingController::class, 'create']);
+
+// Single Listing --always at bottom--
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
