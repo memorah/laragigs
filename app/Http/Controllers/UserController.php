52a5a7fdf65c39ceba00aaa773cc5a9a544eb7bf
memorah <filip.mackov@gmail.com>
2023-06-29 11:55:12 +0200

    login form

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,4 +43,10 @@ class UserController extends Controller
 
         return redirect('/')->with('message', 'You have been logged out"');
     }
+
+    // Show Login Form
+    public function login()
+    {
+        return view('users.login');
+    }
 }
