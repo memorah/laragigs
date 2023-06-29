1cac0a51dfe821456de83a1cba1183d894880719
memorah <filip.mackov@gmail.com>
2023-06-29 11:40:02 +0200

    registration, welcome message

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -38,3 +39,10 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // Single Listing --always at bottom--
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+
+// Show Register/create form 
+Route::get('/register', [UserController::class, 'create']);
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
