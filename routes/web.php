6a833d95ab778f0a9914b8a48092375d0128a855
memorah <filip.mackov@gmail.com>
2023-06-30 16:48:12 +0200

    Make sure logged in user is owner

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,9 @@ Route::put('/listings/{listing}', [ListingController::class, 'update'])->middlew
 // Delete listing
 Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
 
+// Manage Listings
+Route::get('/listings/manage', [ListingController::class, 'manage'])->middleware('auth');
+
 // Single Listing --always at bottom--
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
