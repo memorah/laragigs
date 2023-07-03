8613b4d225821086a2e0a72df060028e86fe7fd0
memorah <filip.mackov@gmail.com>
2023-07-03 11:37:09 +0200

    edit

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -25,7 +25,7 @@
                 },
             };
         </script>
-        <title>LaraGigs | Find Laravel Jobs & Projects</title>
+        <title>LaraGigs | Find Laravel Jobs and Projects</title>
     </head>
     <body class="mb-48">
         <nav class="flex justify-between items-center mb-4">
